10f87c9be879e3fc9d2b84d076cba374d00f4d82
hortencia c <hortenciaecisneros@gmail.com>
2020-08-21 22:52:44 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,22 @@
-# your code goes here
+class Person
+  attr_accessor :bank_account
+  attr_reader :hygiene,:happiness,:name
+  
+  # attr_reader can not be changed
+  # attr_accessor can be changed 
+  
+  def initialize(name)
+    @name = name 
+    @bank_account = $25
+    @happiness = 8
+    @hygiene = 8
+  end
+  def happiness(num)
+    @happiness = num
+    @happiness = 10 if @happiness > 10
+    @happiness = 0 if @happiness < 0
+  end
+
+end
+
+# should return `true` if the happiness or hygiene points exceed seven. Otherwise they should return false.
